dee4053c9ee5223c27a5ad585a6d7cb563192a08
sbx_user1051 <sbx_user1051@169.254.199.73>
2019-08-02 09:54:36 +0000

    Fri Aug 02 09:54:36 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,37 @@
 
 
+SKUS = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15
+}
+
+SPECIAL_OFFERS = {
+    'A': {
+        'amount': 3,
+        'price': 130
+    },
+    'B': {
+        'amount': 2,
+        'price': 45
+    }
+}
+
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    return -1 for any illegal values
+    :param skus: a String containing the SKUs of all the products in the basket
+    :return: an Integer representing the total checkout value of the items
+    """
+    total = 0
+    for sku in skus:
+        try:
+            total += SKUS[sku]
+        except KeyError:
+            return -1
+    return total
+
