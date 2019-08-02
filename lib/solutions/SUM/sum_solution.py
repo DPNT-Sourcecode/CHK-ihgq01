78a0feb01d96c8abd489d4e940049463277dbb40
sbx_user1051 <sbx_user1051@169.254.199.73>
2019-08-02 09:45:24 +0000

    Fri Aug 02 09:45:24 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,10 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    """
+    Sum x and y and return the result
+    :param x: a positive integer between 0-100
+    :param y: a positive integer between 0-100
+    :return: an Integer representing the sum of the two numbers
+    """
+    return x + y
+
